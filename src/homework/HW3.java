6c851812e2a49e19420e2ae45bf42682b436d93c
gdikhsanov <gdikhsanov@gmail.com>
2022-09-14 19:36:45 +0300

    G_mac HW3 #8

--- src/homework/HW3.java
+++ src/homework/HW3.java
@@ -6,27 +6,27 @@ public class HW3 {
 
         int number = 1;
 
-//      #2
+        //      #2
 
-        System.out.println("############################## \n"
+        System.out.println("\n############################## \n"
                 + "Task "
                 + ++number);
 
         byte a = -128;
         byte b = 127;
 
-//      #3
+        //      #3
 
-        System.out.println("############################## \n"
+        System.out.println("\n############################## \n"
                 + "Task "
                 + ++number);
 
         short s = -30_000;
         short t = 30_000;
 
-//      #4
+        //      #4
 
-        System.out.println("############################## \n"
+        System.out.println("\n############################## \n"
                 + "Task "
                 + ++number);
         System.out.println("****************************** \noption 1");
@@ -48,13 +48,77 @@ public class HW3 {
 
         System.out.printf("%s\n|%-20s|%-20d|\n%s\n", line, "Int max value", i, line);
 
-//      #5
+        //      #5
 
-        System.out.println("############################## \n"
+        System.out.println("\n############################## \n"
                 + "Task "
                 + ++number);
        
-        
+        long phoneNumber = 18009998877L;
+
+        //      #6
+
+        System.out.println("\n############################## \n"
+                + "Task "
+                + ++number);
+
+        float f =  100.101101F;
+        double d = 100.101101;
+        line = "---------------------------------------------------------------";
+
+        System.out.printf("%s\n|%-30s|%-30f|\n", line, "float f = 100.101101", f);
+        System.out.printf("%s\n|%-30s|%-30f|\n%s\n", line, "double d = 100.101101", d, line);
+
+
+        //      #7
+        {
+        System.out.println("\n############################## \n"
+                + "Task "
+                + ++number);
+        System.out.println("++++++++++++++++++++++++++++++ \nsubtask a)");
+
+        Double dd = 10.09999 + 20.099999;
+
+        System.out.println("++++++++++++++++++++++++++++++ \nsubtask b)");
+
+        Float ff = new Float(10.09999F + 20.099999F);
+
+        System.out.println("Sum of Double = " + dd);
+        System.out.println("Sum of Float = " + ff);
+        }
+
+        //      #8
+
+        System.out.println("\n############################## \n"
+                + "Task "
+                + ++number);
+
+        float ff = 1F / 3;
+        double dd = 1.0 / (double) 3;
+
+        System.out.println("Division result in float = " + ff);
+        System.out.println("Division result in double = " + dd);
+
+        //      #9
+
+        System.out.println("\n############################## \n"
+                + "Task "
+                + ++number);
+
+        Float floatSum = f + ff;
+        Float floatSub = f - ff;
+        Float floatProduct = f * ff;
+        Float floatQuotient = f / ff;
+        Float floatRemainder = f % ff;
+
+        System.out.println(floatSum);
+        System.out.println(floatSub);
+        System.out.println(floatProduct);
+        System.out.println(floatQuotient);
+        System.out.println(floatRemainder);
+
+
+
 
     }
 }
