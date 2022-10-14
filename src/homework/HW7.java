c2dc0a991197074393aa7aca1776572acf468309
gdikhsanov <gdikhsanov@gmail.com>
2022-10-14 13:50:15 +0300

    G_mac HW7 done + WH 6 the last

--- src/homework/HW7.java
+++ src/homework/HW7.java
@@ -1,9 +1,7 @@
 package homework;
 
-import java.lang.reflect.Array;
+import java.time.Year;
 import java.util.Arrays;
-import java.util.Objects;
-import java.util.Random;
 
 public class HW7 {
 
@@ -11,37 +9,249 @@ public class HW7 {
         System.out.println("\n*********************\nTask # " + task + "\n");
     }
 
+    public static String returnMonth(int number) {
+        if (number > 0 && number < 13) {
+            switch (number) {
+                case 1:
+                    return "Jan";
+                case 2:
+                    return "Feb";
+                case 3:
+                    return "Mar";
+                case 4:
+                    return "Apr";
+                case 5:
+                    return "May";
+                case 6:
+                    return "Jun";
+                case 7:
+                    return "Jul";
+                case 8:
+                    return "Aug";
+                case 9:
+                    return "Sep";
+                case 10:
+                    return "Oct";
+                case 11:
+                    return "Nov";
+                case 12:
+                    return "Dec";
+            }
+        }
+        return "Error";
+    }
+
+    public static String returnDayOfTheWeek(int number) {
+        if (number > 0 && number < 8) {
+
+            switch (number) {
+                case 1:
+                    return "Mon";
+                case 2:
+                    return "Tue";
+                case 3:
+                    return "Wed";
+                case 4:
+                    return "Thu";
+                case 5:
+                    return "Fri";
+                case 6:
+                    return "Sat";
+                case 7:
+                    return "Sun";
+            }
+        }
+        return "Error";
+    }
+
+    public static int returnDayOfTheWeek(String day) {
+
+        switch (day) {
+            case "Mon":
+                return 1;
+            case "Tue":
+                return 2;
+            case "Wed":
+                return 3;
+            case "Thu":
+                return 4;
+            case "Fri":
+                return 5;
+            case "Sat":
+                return 6;
+            case "Sun":
+                return 7;
+        }
+
+        return 0;
+    }
+
+    public static int returnDate(int date) {
+        if (date < 10) {
+            String number = String.format("%03d", date);
+
+            return Integer.valueOf(number); //добавляем ноль для 1-9
+        } else {
+            return date;
+        }
+    }
+
+    public static int daysInMonth(int month, int year) {
+
+        int numDays;
+
+        switch (month) {
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
+                numDays = 31;
+                break;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                numDays = 30;
+                break;
+            case 2:
+                if (((year % 4 == 0) &&
+                        !(year % 100 == 0))
+                        || (year % 400 == 0))
+                    numDays = 29;
+                else
+                    numDays = 28;
+                break;
+            default:
+
+                return -1;
+        }
+
+        return numDays;
+    }
+
+    public static void printEightDaysFromDate(String day, int month, int date) {
+
+        int year = Year.now().getValue();
+        int currentYear = Year.now().getValue();
+
+        if (returnDayOfTheWeek(day) == 0 ||
+                returnMonth(month).equals("Error") ||
+                month <= 0 ||
+                date <= 0 ||
+                date > (daysInMonth(month, year))) {
+
+            System.out.println("Please enter correct data.");
+
+        } else {
+            int currentDay = returnDayOfTheWeek(day);
+            int currentMonth = month;
+            int currentDate = date;
+//day
+            for (int i = 0; i <= 7; i++) {
+                if (currentDay != 7) {
+                    System.out.print(returnDayOfTheWeek(currentDay) + ", ");
+                    currentDay++;
+                } else {
+                    System.out.print(returnDayOfTheWeek(currentDay) + ", ");
+                    currentDay = 1;
+                }
+//month + date
+                if (currentDate <= daysInMonth(currentMonth, currentYear)) {
+                    System.out.println(returnMonth(currentMonth) + " " + returnDate(currentDate));
+//                    System.out.println(" " + currentYear); //проверка года - закоментить
+                    currentDate++;
+                } else {
+                    if (currentMonth != 12) {
+                        currentMonth++;
+                    } else {
+                        currentMonth = 1;
+                        currentYear = year + 1;
+                    }
+                    currentDate = 1;
+                    System.out.println(returnMonth(currentMonth) + " " + returnDate(currentDate));
+//                    System.out.println(" " + currentYear); //проверка года - закоментить
+                    currentDate++;
+                }
+            }
+        }
+//        РАЗОБРАТЬСЯ   (выше было int l = 8:)
+//        else {
+//            for (int i = returnDayOfTheWeek(day); i <= 7 && l > 0; i++, date++, l--) {
+//                if (i != 7) {
+//                    if ((month <= 7 && month % 2 != 0 && returnDate(date) >= 32)
+//                            || ((month == 9 || month == 11) && returnDate(date) >= 31)
+//                            || ((month == 4 || month == 6) && returnDate(date) >= 31)
+//                            || (month >= 8 && month <= 12 && month % 2 == 0 && returnDate(date) >= 32)
+//                            || (month == 2 && returnDate(date) >= 29)) {
+//                        date = 1;
+//                        month += 1;
+//                    }
+//
+//                    System.out.println(returnDayOfTheWeek(i) + ", " + returnMonth(month) + " " + returnDate(date));
+//                } else {
+//                    System.out.println(returnDayOfTheWeek(i) + ", " + returnMonth(month) + " " + returnDate(date));
+//                    i = 0;
+//                }
+//            }
+//        }
+    }
+
+    //2
+    public static void siqenceFromToStep(int from, int to, int step) {
+        if (step != 0 && from <= to) {
+            for (int i = from; i <= to; i++) {
+                if (i % Math.abs(step) == 0) {
+                    System.out.print(i + " ");
+                }
+            }
+
+        } else if (step != 0 && from > to) {
+            for (int i = from; i >= to; i--) {
+                if (i % Math.abs(step) == 0) {
+                    System.out.print(i + " ");
+                }
+            }
+
+        } else {
+            System.out.println("Error: step is zero exception");
+        }
+    }
+
+
     //21
-     public static void printEven(int l) {
+    public static void printEven(int l) {
 
         int[] numbers = new int[l];
 
 
         for (int i = 0; i < l; i++) {
-             numbers[i] = (int)(Math.random() * 7);
-         }
+            numbers[i] = (int) (Math.random() * 7);
+        }
 
-         System.out.print(Arrays.toString(numbers));
+        System.out.print(Arrays.toString(numbers));
 
-     }
+    }
 
-     //22
+    //22
     public static void printAerage(int[] arr) {
         int sum = 0;
         for (int i = 0; i < arr.length; i++) {
             sum += arr[i];
         }
-        System.out.println(/*Math.round*/(sum / (double)arr.length));
+        System.out.println(/*Math.round*/(sum / (double) arr.length));
     }
 
-     //25
+    //25
     public static void checkArr(int[] arr, int number) {
 
         int maybe = 0;
 
         for (int i = 0; i < arr.length; i++) {
 
-            if (arr[i] == number + 1 || arr[i] == number - 1 ) {
+            if (arr[i] == number + 1 || arr[i] == number - 1) {
                 System.out.println("maybe");
                 maybe += 1;
             }
@@ -57,9 +267,17 @@ public class HW7 {
 
     public static void main(String[] args) {
 
+        taskNumber(1);
+
+        printEightDaysFromDate("Fri", 12, 28);
+
+        taskNumber(2);
+
+        siqenceFromToStep(-11, -61, 15);
+
         taskNumber(3);
 
-        String[] catsNames = {"q" ,"w" ,"e" ,"r" ,"t" ,"y", "u"};
+        String[] catsNames = {"q", "w", "e", "r", "t", "y", "u"};
 
         taskNumber(4);
 
@@ -88,8 +306,8 @@ public class HW7 {
 
         taskNumber(9);
 
-        for (String c: catsColors
-             ) {
+        for (String c : catsColors
+        ) {
             if (c.equalsIgnoreCase("grey")) {
                 System.out.println("Накорми кота!");
             }
@@ -97,17 +315,17 @@ public class HW7 {
 
         taskNumber(10);
 
-        for (boolean c: isCatRed
+        for (boolean c : isCatRed
         ) {
             if (c == true) {
                 System.out.println("Дай коту воды!");
             }
         }
-        
+
         taskNumber(11);
 
-        for (int c: catsAges
-             ) {
+        for (int c : catsAges
+        ) {
             if (c < 2) {
                 System.out.println("Отнеси кота на прививку!");
             }
@@ -116,9 +334,9 @@ public class HW7 {
         taskNumber(12);
 
         System.out.printf("%s %s %d",
-                catsNames[catsNames.length -1],
-                catsNames[catsColors.length -1],
-                catsAges[catsNames.length -1]);
+                catsNames[catsNames.length - 1],
+                catsNames[catsColors.length - 1],
+                catsAges[catsNames.length - 1]);
 
         taskNumber(13);
 
@@ -128,7 +346,7 @@ public class HW7 {
                 catsAges[catsAges.length / 2],
 
                 catsNames[catsNames.length / 2 + 1],
-                catsColors[catsColors.length / 2 +1],
+                catsColors[catsColors.length / 2 + 1],
                 catsAges[catsAges.length / 2 + 1]);
 
         taskNumber(14);
@@ -153,7 +371,7 @@ public class HW7 {
         for (int i = 0; i < catsAges.length; i++) {
             sumAge += catsAges[i];
         }
-        System.out.println(/*Math.round*/(sumAge / (double)catsAges.length));
+        System.out.println(/*Math.round*/(sumAge / (double) catsAges.length));
 
         taskNumber(17);
 
@@ -197,7 +415,7 @@ public class HW7 {
         taskNumber(20);
 
         for (int i = 0; i < catsNames.length; i++) {
-            if (i % 2 !=0 && catsAges[i] < 2) {
+            if (i % 2 != 0 && catsAges[i] < 2) {
                 System.out.println(catsNames[i]);
             }
         }
@@ -213,12 +431,12 @@ public class HW7 {
         taskNumber(23);
 
         int[] odd = new int[50];
-        int j = 0;
+        int o = 0;
 
         for (int i = -1000; i < -900; i++) {
             if (i % 2 != 0) {
-                odd[j] = i;
-                j += 1;
+                odd[o] = i;
+                o += 1;
             }
         }
         System.out.println(Arrays.toString(odd));
@@ -228,14 +446,14 @@ public class HW7 {
         int[] numbers = new int[10];
 
         for (int i = 0; i < 10; i++) {
-            numbers[i] = (int)(Math.random() * 101)  + 100;
+            numbers[i] = (int) (Math.random() * 101) + 100;
         }
 
         System.out.print(Arrays.toString(numbers));
 
         taskNumber(25);
 
-        checkArr(new int[]{1,2,1,4,5,2,6,4,1,2,1,4,3,5,3,2,8,4}, 3);
+        checkArr(new int[]{1, 2, 1, 4, 5, 2, 6, 4, 1, 2, 1, 4, 3, 5, 3, 2, 8, 4}, 3);
 
         taskNumber(26);
         //numbers array from task 24
@@ -258,8 +476,7 @@ public class HW7 {
             if (numbers[i] % 2 == 0) {
                 evenNumbers[evenIndex] = numbers[i];
                 evenIndex += 1;
-            }
-            else {
+            } else {
                 oddNumbers[oddIndex] = numbers[i];
                 oddIndex += 1;
             }
@@ -270,9 +487,45 @@ public class HW7 {
         System.out.println(Arrays.toString(oddNumbers));
 
         taskNumber(27);
+                /*
+                {{"Мурзик", 2, "Grey"}, {"Черныш", 3, "Black"}, {"Мурка", 4, "Grey"},
+                {"Васька", 1, "Brown"}, {"Рыжик", 5, "Red"}, {"Дымка", 6, "Grey"}, {"Патрик", 9, "Red"},
+                {"Зорро", 10, "Grey"}};
+                */
 
-        Object[][] cats = new Object[][]{{"Мурзик", 2, "Grey"}, {"Черныш", 3, "Black"}, {"Мурка", 4, "Grey"},
-                {"Васька", 1, "Brown"}};
+        Object[][] cats =
+                {{"Мурзик", "Черныш", "Мурка", "Васька", "Рыжик", "Дымка", "Патрик", "Зорро"},
+                        {2, 3, 4, 1, 5, 6, 9, 10},
+                        {"Grey", "Black", "Grey", "Brown", "Red", "Grey", "Red", "Grey"}};
+
+        for (int i = 0; i < cats[1].length; i++) {
+            if (i % 2 == 0) {
+                System.out.println(cats[0][i] + " " + cats[1][i] + " " + cats[2][i]);
+            }
+        }
+
+        taskNumber(28);
+
+        int[][] randomArr = new int[4][8];
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 8; j++) {
+                randomArr[i][j] = (int) (Math.random() * 10) + 1;
+            }
+        }
+        System.out.println(Arrays.deepToString(randomArr));
+
+        taskNumber(29);
+
+        for (int i = 0; i < randomArr.length; i++) {
+            for (int j = 0; j < randomArr[i].length; j++) {
+                if (randomArr[i][j] % 2 == 0) {
+                    System.out.print(randomArr[i][j] + " ");
+                }
+            }
+        }
+
+        taskNumber(30);
 
 
     }
